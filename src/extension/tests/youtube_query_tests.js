ea890811645edafe064b38a9c10efd996595ca44
matthewoflynn <matthewoflynn@gmail.com>
2015-11-07 15:23:16 +0000

    Updating the unit tests

--- src/extension/tests/youtube_query_tests.js
+++ src/extension/tests/youtube_query_tests.js
@@ -3,39 +3,39 @@ module("Youtube Search Query Module");
 asyncTest("Search results are valid", function() {
 	var query = new YoutubeSearchQuery();
 	
-	query.setResponseHandler( function(results) {
-		equals(results.search_query, "query", "Result query and actual query don't match");
+	query.setResponseHandler( function(search_query, results) {
+		equals(search_query, "query", "Result query string and actual query string don't match");
 		ok(results.total_results > 0, "Result count is not > 0");
-		equals(results.entries.length, 10, "Result entries count is not 10");
+		equals(results.entries.length, 5, "Result entries count is not 5");
 		
 		start();
 	});
 	
-	query.executeQuery("query", 1, 10);
+	query.executeQuery("query", 1, 5);
 });
 
 asyncTest("Query can yield no results", function() {
 	var query = new YoutubeSearchQuery();
 	
-	query.setResponseHandler( function(results) {
+	query.setResponseHandler( function(search_query, results) {
 		equals(results.total_results, 0, "Result count is 0");
 		
 		start();
 	});
 	
-	query.executeQuery("asdfkjweacsdkl", 1, 10);
+	query.executeQuery("asdfkjweacsdkl", 1, 5);
 });
 
 asyncTest("Blank query yields no results", function() {
 	var query = new YoutubeSearchQuery();
 	
-	query.setResponseHandler( function(results) {
+	query.setResponseHandler( function(search_query, results) {
 		equals(results.total_results, 0, "Result count should be 0");
 		
 		start();
 	});
 	
-	query.executeQuery("", 1, 10);
+	query.executeQuery("", 1, 5);
 });
 
 /*
--- src/extension/youtube_query.js
+++ src/extension/youtube_query.js
@@ -37,6 +37,12 @@ function YoutubeSearchQuery() {
 	
 	this.executeQuery = function(search_string, start_index, results_per_page) {
 		_search_string = search_string;
+
+		if (search_string === "") {
+			handle_empty_query();
+			return;
+		}
+
 		var youtube = {
 			BaseApiQuery : "https://www.googleapis.com/youtube/v3",
 			ApiQuery : "/search?q=" + search_string,
@@ -54,6 +60,15 @@ function YoutubeSearchQuery() {
 	};
 	
 	/** Private Methods *****************************************************/
+	var handle_empty_query = function() {
+		var results = {
+			total_results : 0,
+			entries : []
+		};
+
+		_response_handler("", results);
+	}
+
 	var sendRequest = function(http_request) {
 		var xml_http_request = new XMLHttpRequest();
 		
@@ -83,15 +98,9 @@ function YoutubeSearchQuery() {
 	};
 	
 	var convertResponseTextIntoObject = function(response_text) {	
-				
-		var results = {};
-		
-		
-		
-		// parse results
 		var data = JSON.parse(response_text);	
 		
-		results.search_query = _search_string;
+		var results = {};
 		results.total_results = data.pageInfo.totalResults;
 		results.entries = [];
 		
@@ -100,16 +109,16 @@ function YoutubeSearchQuery() {
 		
 		for (var i = 0; i < entries.length; ++i) {
 			var entry = entries[i];
-			var video_id = entry;
+			var video_id = entry.id.videoId;
 			var result = {};
 			result = {
 						id:				video_id,
 						title: 			entry.snippet.title.substring(42),
 						views:			5,
 						duration: 		5,
-						link_url: 		"www.google.com",
+						link_url: 		"https://www.youtube.com/watch?v=" + video_id,
 						thumb_url:		entry.snippet.thumbnails.default.url,
-						content_url: 	"www.google.com"
+						content_url: 	"http://www.google.com"
 					  };
 			
 			var mins = Math.floor(result.duration / 60);
